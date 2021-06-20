65edf3d922dbc13352360302f7d438293dd62b08
Name <myemail@gmail.com>
2021-06-20 13:03:30 +0100

    add products array

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ Written by Med Reda Kamal
 
 const firstCartDOM = document.getElementById("cart1");
 const secondCartDOM = document.getElementById("cart2");
+
+const products = [{
+    name: "Apple Iphone 12",
+    price: 799,
+    qty: 3,
+},
+{
+    name: "Sony Playsation 5",
+    price: 499,
+    qty: 7,
+},
+];
