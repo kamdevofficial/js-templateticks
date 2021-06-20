7d189380be3d9af07e7c5dd4c5437795f66bae06
Name <myemail@gmail.com>
2021-06-20 13:11:49 +0100

    add quantity, price

--- script.js
+++ script.js
@@ -79,13 +79,25 @@ renderCart1();
 // * Method 2 : using Template Literals ! Magical way *
 
 const renderCart2 = () => {
-    cartContent = `<h2>Cart - Example 2</h2>
-    <ul>
+    cartContent = `
+  <h2>Cart - Example 2</h2>
+  <ul>
     ${products
       .map(
         (product) =>
           `<li>${product.name} - $${product.price} - ${product.qty} item(s)</li>`
       )
       .join("")}
-  </ul>`;
+  </ul>
+  <div>
+   <span>Quantity : ${products.reduce(
+     (acc, product) => acc + product.qty,
+     0
+   )} item(s)</span></br>
+   <span>You will pay : $${products.reduce(
+     (acc, product) => acc + product.price,
+     0
+   )}</span>
+  </div>
+  `;
 };
